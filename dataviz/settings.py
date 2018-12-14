688c6cec64baff7b79046d20881d5ac53f8ea0f9
Jasparr77 <jjcroome@gmail.com>
2018-12-14 05:43:33 -0800

    template fix

--- dataviz/settings.py
+++ dataviz/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dataviz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'vizzes/templates/vizzes/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
