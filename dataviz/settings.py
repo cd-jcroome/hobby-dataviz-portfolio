f0cd1c59e838662a9bfe1435f2736e405e92e06e
Jasparr77 <jjcroome@gmail.com>
2018-12-21 05:52:47 -0800

    morning try

--- dataviz/settings.py
+++ dataviz/settings.py
@@ -139,8 +139,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
 
 django_heroku.settings(locals())
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
