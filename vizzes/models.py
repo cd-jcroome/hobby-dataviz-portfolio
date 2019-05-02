97d9a69577a5533b774e7ba19d4bdf1e3c3f1bca
Jasparr77 <jjcroome@gmail.com>
2019-05-02 06:51:54 -0700

    model flexibility
    multimedia conglomerate entertainment 7Jasper

--- vizzes/models.py
+++ vizzes/models.py
@@ -4,13 +4,25 @@ from django.db import models
 
 class Dataviz(models.Model):
     """Model definition for Dataviz."""
-
+    D3_SCROLL = 'D3L'
+    D3_STATIC = 'D3T'
+    WORDS = 'WRD'
+    TABLEAU = 'TBX'
+    IMAGE = 'IMG'
+    VIZ_TYPE_CHOICES = (
+        (D3_SCROLL,'D3 w/ Scrollytelling'),
+        (D3_STATIC,'Static D3'),
+        (TABLEAU,'Embedded Tableau Workbook'),
+        (IMAGE,'Image'),
+        (WORDS,'Just Words')
+    )
     viz_name = models.CharField(max_length=500,unique=True)
+    viz_type  = models.CharField(max_length=3,choices=VIZ_TYPE_CHOICES,default=D3_STATIC)
     viz_verbose_name = models.CharField(max_length=500, default='---')
     date_added = models.DateTimeField('Date Added')
     viz_description = models.TextField(default='A brief Description of the Viz')
     viz_source = models.URLField(default='Data Source')
-    viz_file = models.URLField(max_length=500, default='https://cdn.jsdelivr.net/gh/jasparr77/d3/[Folder]/[JS File]')
+    viz_file = models.CharField(max_length=1000, default='Paste CDN URL or TBX embed code here')
     viz_thumb = models.URLField(default='https://cdn.jsdelivr.net/gh/jasparr77/d3/[Folder]/[PNG File]')
     author = models.CharField(max_length=25, default='')
     class Meta:
